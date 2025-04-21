328274113e1d081bf76c8db7825b7159b7bd0a53
leehendrick <imhendrick16@gmail.com>
2025-04-20 22:43:53 -0400

    chore: import prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import  { UserModule } from "./modules/user/user.module";
 import  { ReportModule } from "./modules/report/report.module";
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ReportModule],
+  imports: [AuthModule, UserModule, ReportModule, PrismaModule],
   controllers: [],
   providers: [],
 })
