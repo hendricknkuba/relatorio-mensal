78cea4ea2e44756d66ae21c79e4d5a8d99434175
leehendrick <imhendrick16@gmail.com>
2025-04-21 18:47:46 -0400

    refactor: use try catch an & customize  error

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,25 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
+    try {
+      const hash = await argon.hash(dto.password);
 
-    // save the user in database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hash,
-        name: dto.name,
-      },
-    });
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+          name: dto.name,
+        },
+      });
 
-    return user;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // Verify prisma duplicate field error code
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken.');
+        }
+
+        throw error;
+      }
+    }
   }
 
   signin() {
