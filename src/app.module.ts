4e49386db24c01275224079a3a32e1856ebc350e
leehendrick <imhendrick16@gmail.com>
2025-04-20 21:20:12 -0400

    chore: import new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import  { UserModule } from "./modules/user/user.module";
+import  { ReportModule } from "./modules/report/report.module";
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ReportModule],
   controllers: [],
   providers: [],
 })
