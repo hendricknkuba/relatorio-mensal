ac25559f090abc4a0a069a5f1ee651d388578f57
leehendrick <imhendrick16@gmail.com>
2025-04-21 21:51:40 -0400

    chore: register config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
-import  { UserModule } from "./modules/user/user.module";
-import  { ReportModule } from "./modules/report/report.module";
+import { UserModule } from './modules/user/user.module';
+import { ReportModule } from './modules/report/report.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ReportModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    ReportModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
